4101d0d6751cfaa7bee424666ff80d8a6e590763
Tommi S.E. Laukkanen <tommi.s.e.laukkanen@gmail.com>
2018-04-24 21:40:05 +0300

    Added @Api annotation to TestResource.

--- src/main/java/org/bubblecloud/starter/TestResource.java
+++ src/main/java/org/bubblecloud/starter/TestResource.java
@@ -1,11 +1,14 @@
 package org.bubblecloud.starter;
 
+import io.swagger.annotations.Api;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.HttpHeaders;
 
+@Api
 @Path("/test")
 public class TestResource {
 
